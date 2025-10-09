445a4a8d7692dcd6acb6f4ab76dec6562be55424
Panchali Samarasinghe <panchalisam20030117@gmail.com>
2025-10-09 23:32:43 +0530

    Complete all basic ui

--- app/src/main/java/lk/voltgo/voltgo/navigation/Screen.kt
+++ app/src/main/java/lk/voltgo/voltgo/navigation/Screen.kt
@@ -11,8 +11,11 @@ sealed class Screen(val route: String) {
     // Main Flow - EV Owner
     data object Home : Screen("home")
     data object MyReservations : Screen("my_reservations")
+    data object NewReservation : Screen("new_reservation")
+    data object Stations : Screen("stations")
 
     // Operator Flow
+    data object OperatorHome : Screen("operator_home")
 
 }
 // Navigation graphs
